39147b514cf6ecda9bbf0b014628bb90e60ed932
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2018-01-26 14:09:08 +0100

    email input added

---
+++ addon/components/mi-email.js
@@ -0,0 +1,39 @@
+import layout from '../templates/components/mi-email';
+import MobileInputComponentMixin from "../mixins/mobile-input-component";
+import { get } from '@ember/object';
+import { getWithDefault } from '@ember/object';
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+
+export default Component.extend(MobileInputComponentMixin, {
+  layout,
+  tagName: 'span',
+  onValueChanged() {},
+  oldValue: null,
+
+  init() {
+    this._super(...arguments);
+    this.oldValue = this.value;
+  },
+
+  placeholder: computed('formattedPlaceholder', 'disabled', function() {
+    if (get(this, 'disabled')) {
+      return "";
+    } else {
+      return getWithDefault(this, 'formattedPlaceholder', "");
+    }
+  }),
+
+  actions: {
+    onKeyUp() {
+      let newValue = this.get('value');
+      if (newValue === this.get('oldValue')) {
+        //this happens eg. when input is empty and user presses backspace
+        return;
+      }
+      this.set('oldValue', newValue);
+      this.get('onValueChanged')(newValue);
+    }
+  }
+});
