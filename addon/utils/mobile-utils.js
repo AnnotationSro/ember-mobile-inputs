7cc0f419c5e52d547e98239e6e877503534b4140
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2020-10-08 16:52:46 +0200

    remove deprecated getWithDefault

--- addon/utils/mobile-utils.js
+++ addon/utils/mobile-utils.js
@@ -1,7 +1,19 @@
+import {
+  get
+} from '@ember/object';
+
 export function isTouchDevice() {
-  if ('callPhantom' in window){
+  if ('callPhantom' in window) {
     //if running PhantomJS tests, make this a non-touch device
     return false;
   }
   return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+}
+
+export function getWithDefault(obj, key, defaultValue) {
+  let result = get(obj, key);
+  if (typeof result === 'undefined') {
+    result = defaultValue;
+  }
+  return result;
 }
